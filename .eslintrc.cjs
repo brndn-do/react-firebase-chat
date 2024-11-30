50d67f459a219cd53ee0ea3a80fd254b466e1533
brndn-do <dobrandon05@gmail.com>
2024-11-30 13:01:10 -0600

    Develop ChatList component
    Made plus icon next to searchbar dynamic, added user items underneath
    search bar and styled them. Styled chatlist container for scrolling.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "no-unused-vars": "warn",
     'react/jsx-no-target-blank': 'off',
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,9 @@
 import "./chatList.css";
+import { useState } from "react";
 
 const ChatList = () => {
+  // state for a dyanmic icon
+  const [plusMode, setPlusMode] = useState(false);
   return (
     <div className="chatList">
       <div className="search">
@@ -8,7 +11,48 @@ const ChatList = () => {
           <img src="/search.png" alt="" />
           <input type="text" placeholder="Search" />
         </div>
-        <img src="/plus.png" alt="" className="plus" />
+        {/* dynamic icon */}
+        <img
+          src={plusMode ? "/minus.png" : "/plus.png"}
+          alt=""
+          className="plus"
+          onClick={() => setPlusMode((prevState) => !prevState)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello </p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello </p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello </p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello </p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello </p>
+        </div>
       </div>
     </div>
   );
