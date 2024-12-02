6614d8611c5b5edd18e2ac68be1e7b6699cd809e
brndn-do <dobrandon05@gmail.com>
2024-12-02 17:45:23 -0600

    Add login and sign up form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,54 @@
 import "./login.css";
 
+import { useState } from "react";
+
 const Login = () => {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+
+  const handleAvatar = (event) => {
+    if (event.target.files[0]) {
+      setAvatar({
+        file: event.target.files[0],
+        url: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
   return (
-    <div className="login">Login</div>
-  )
+    <div className="login">
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="text" placeholder="Password" name="password" />
+          <button>Sign in</button>
+        </form>
+      </div>
+      <div className="separator"></div>
+      <div className="item">
+        <h2>Create an Account</h2>
+        <form>
+          <label htmlFor="file">
+            <img src={avatar.url ? avatar.url : "/avatar.png"} alt="" />
+            Upload an image
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="text" placeholder="Password" name="password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
