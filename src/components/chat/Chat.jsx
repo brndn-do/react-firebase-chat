22a1a45669c5f8c8a709cfe25e45a4bf1185613a
brndn-do <dobrandon05@gmail.com>
2024-12-02 01:17:46 -0600

    Add auto scroll after initial chat component load

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior : "instant" })
+  }, []);
 
   const handleEmoji = (event) => {
     setText(prev => prev + event.emoji);
@@ -68,6 +74,7 @@ const Chat = () => {
             <span>1m ago</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
       <div className="bottom">
         <div className="icons">
